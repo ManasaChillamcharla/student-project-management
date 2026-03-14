b12ca182427c7ea80dc7fd2ddc9e613e2f209f80
ManasaChillamcharla <23eg105v31@anurag.edu.in>
2026-03-14 11:57:22 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://student-project-management-9gjm.onrender.com",
 });
 
 export default API;
